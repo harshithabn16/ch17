e7338b2e21941025c83520c5990f7c157bfcad91
Harshitha <harshithabn2003@gmail.com>
2024-07-19 12:23:30 +0530

    trying to Push

---
+++ Fan.java
@@ -0,0 +1,10 @@
+Class Fan {
+public static game(byte no,int value, string name, short key)
+{
+System.out.println("Strating in game method");
+System.out.println("No : "+no);
+System.out.println("value : "+value);
+System.out.println("Name : "+name);
+System.out.println("key : "+key);
+}
+}
