43de0b3fe6029ec61bf081f1da3daba9e1bb00da
Harshitha <harshithabn2003@gmail.com>
2024-07-30 22:14:04 +0530

    JAVA PROGRAMS

---
+++ methods/Fish.java
@@ -0,0 +1,153 @@
+//Assignment 23 :
+
+// Declare two instance variables and init it using const
+//Create a minimum of 11 instances
+//Fish
+//Lamp
+//PrintingMachine
+//Lens
+//Coil
+
+//Declare three instance variables and init it using const
+//Cap
+//Gold
+//Board
+//Led
+//SimCard
+
+class Fish {
+	String name;
+	String size;
+	
+	public Fish(String nameLocal,String sizeLocal)
+	{
+		System.out.println("Created Fish");
+		System.out.println("The Fish Name  : "+nameLocal);
+		System.out.println("The Fish Size   : "+sizeLocal);
+	}
+}
+
+class Lamp {
+	String company;
+    double  prize;
+	
+	public Lamp(String companyLocal,double prizeLocal)
+	{
+		System.out.println("Created Lamp");
+		System.out.println("The Company Name  : "+companyLocal);
+		System.out.println("The  Prize  : "+prizeLocal);
+	}
+}
+
+class PrintingMachine {
+	String company;
+	double prize;
+	
+	public PrintingMachine(String companyLocal,double prizeLocal)
+	{
+		System.out.println("Created PrintingMachine");
+		System.out.println("The Company Name    : "+companyLocal);
+		System.out.println("The Fish Prize  : "+prizeLocal);
+	}
+}
+
+class Lens {
+	String name;
+	int quality;
+	
+	public Lens(String nameLocal,int qualityLocal)
+	{
+		System.out.println("Created Lens");
+		System.out.println("The Lens Name  : "+nameLocal);
+		System.out.println("The  quality   : "+qualityLocal);
+	}
+}
+
+class Coil {
+	String name;
+	double prize;
+	
+	public Coil(String nameLocal,double prizeLocal)
+	{
+		System.out.println("Created Coil");
+		System.out.println("The Coil Name  : "+nameLocal);
+		System.out.println("The  Prize   : "+prizeLocal);
+	}
+}
+
+//Declare three instance variables and init it using const
+//Cap
+//Gold
+//Board
+//Led
+//SimCard
+
+class Cap {
+	String name;
+	String size;
+	double prize;
+	
+	public Cap(String nameLocal,String sizeLocal,double prizeLocal)
+	{
+		System.out.println("Created Cap");
+		System.out.println("The Cap Name  : "+nameLocal);
+		System.out.println("The Cap Size   : "+sizeLocal);
+		System.out.println("The Prize   : "+prizeLocal);
+	}
+}
+
+class Gold {
+	String name;
+	int carat;
+	double prize;
+	
+	public Gold(String nameLocal,int caratLocal,double prizeLocal)
+	{
+		System.out.println("Created Gold");
+		System.out.println("The Gold Name  : "+nameLocal);
+		System.out.println("The Gold carat   : "+caratLocal);
+		System.out.println("The Prize   : "+prizeLocal);
+	}
+}
+
+class Board {
+	String name;
+	String size;
+	double prize;
+	
+	public Board(String nameLocal,String sizeLocal,double prizeLocal)
+	{
+		System.out.println("Created Board");
+		System.out.println("The Board Name  : "+nameLocal);
+		System.out.println("The Board Size   : "+sizeLocal);
+		System.out.println("The Prize   : "+prizeLocal);
+	}
+}
+
+class Led {
+	String name;
+	String size;
+	String colour;
+	
+	public Led(String nameLocal,String sizeLocal,String colourLocal)
+	{
+		System.out.println("Created Led");
+		System.out.println("The Led Name  : "+nameLocal);
+		System.out.println("The Led Size   : "+sizeLocal);
+		System.out.println("The Led Colour   : "+colourLocal);
+	}
+}
+
+class SimCard {
+	String name;
+	String size;
+	double prize;
+	
+	public SimCard(String nameLocal,String sizeLocal,double prizeLocal)
+	{
+		System.out.println("Created SimCard");
+		System.out.println("The SimCard Name  : "+nameLocal);
+		System.out.println("The SimCard Size   : "+sizeLocal);
+		System.out.println("The SimCard Prize : "+prizeLocal);
+	}
+}
---
+++ methods/FishRunner.java
@@ -0,0 +1,101 @@
+//Assignment 23 :
+
+class FishRunner {
+public static void main(String[] args)
+{
+System.out.println("Start in FishRunner");
+
+Fish fish=new Fish("Bangdi","Large");
+Fish fish1=new Fish("Star Fish","Large");
+Fish fish2=new Fish("Flying Fish","Small");
+Fish fish3=new Fish("Swordtails","Midium");
+Fish fish4=new Fish("Gold Fish","Small");
+Fish fish5=new Fish("Raindow Fish","Large");
+Fish fish6=new Fish("Betta Fish","Midium");
+Fish fish7=new Fish("Oscar Fish","Large");
+Fish fish8=new Fish("Angel Fish","Large");
+Fish fish9=new Fish("Zebra","Small");
+Fish fish10=new Fish("Snake Fish","Midium");
+
+System.out.println("---------------------------------------");
+
+Lamp lamp=new Lamp("Eclipse Lighting",49.0);
+Lamp lamp1=new Lamp("Lumina Lux",89.0);
+Lamp lamp2=new Lamp("Shine & Shade",23.0);
+Lamp lamp3=new Lamp("Gleam Design",59.0);
+Lamp lamp4=new Lamp("Spark Lightworks",90);
+Lamp lamp5=new Lamp("IllumaCraft",30);
+Lamp lamp6=new Lamp("Aura Luminaries",28.9);
+Lamp lamp7=new Lamp("Twilight Glimmer",49.29);
+Lamp lamp8=new Lamp("Beacon Bliss",48.0);
+Lamp lamp9=new Lamp("Lustre Lamps",19.0);
+Lamp lamp10=new Lamp("Celestial Glow",60.0);
+
+System.out.println("---------------------------------------");
+
+PrintingMachine printingMachine=new PrintingMachine("PrintPro Solutions",45000);
+PrintingMachine printingMachine1=new PrintingMachine("Precision PrintTech",95000);
+PrintingMachine printingMachine2=new PrintingMachine("PrimePrint Systems",80000);
+PrintingMachine printingMachine3=new PrintingMachine("InkMasters",29000);
+PrintingMachine printingMachine4=new PrintingMachine("ProPrint Machinery",95000);
+PrintingMachine printingMachine5=new PrintingMachine("InnovaPrint",86000);
+PrintingMachine printingMachine6=new PrintingMachine("PrintForge",50000);
+PrintingMachine printingMachine7=new PrintingMachine("PrintFusion",25000);
+PrintingMachine printingMachine8=new PrintingMachine("PrintSphere",87000);
+PrintingMachine printingMachine9=new PrintingMachine("Inker Technologies",98000);
+PrintingMachine printingMachine10=new PrintingMachine("OptiPrint Systems",76000);
+
+
+System.out.println("---------------------------------------");
+
+Lens lens=new Lens("CrystalView Optics",56);
+Lens lens1=new Lens("PrecisionLens",78);
+Lens lens2=new Lens("LuminousFocus",96);
+Lens lens3=new Lens("ClarityCraft",39);
+Lens lens4=new Lens("PrimeOptics",28);
+Lens lens5=new Lens("Visionary Glass",88);
+Lens lens6=new Lens("PerfectFocus",79);
+Lens lens7=new Lens("RefinedLens",68);
+Lens lens8=new Lens("AccuraLens",76);
+Lens lens9=new Lens("FocusFusion",36);
+Lens lens10=new Lens("VividLens",99);
+
+System.out.println("---------------------------------------");
+
+Coil coil=new Coil("CoilCraft",563);
+Coil coil1=new Coil("PrecisionCoils",999);
+Coil coil2=new Coil("EliteWinding",833);
+Coil coil3=new Coil("PureSpiral",287);
+Coil coil4=new Coil("DynamiCoil",937);
+Coil coil5=new Coil("OptiWind Coils",198);
+Coil coil6=new Coil("Vortex Coils",439);
+Coil coil7=new Coil("FlexiWind",923);
+Coil coil8=new Coil("PrimeSpiral",280);
+Coil coil9=new Coil("SpiralPro",600);
+Coil coil10=new Coil("AccelCoil",250);
+
+
+System.out.println("---------------------------------------");
+
+
+Cap cap=new Cap("PeakWear","Large",100);
+
+System.out.println("---------------------------------------");
+
+Gold gold=new Gold("Necklace",24,1000000);
+
+System.out.println("---------------------------------------");
+
+Board board=new Board("Green Board","Large",50500);
+
+System.out.println("---------------------------------------");
+
+Led led=new Led("Doms","Small","Black");
+
+System.out.println("---------------------------------------");
+
+SimCard simCard=new SimCard("jio","Midium",299);
+
+System.out.println("Ends in FishRunner");
+}
+}
