8a23f4d3b7a79d1b30782f100a42eecf7cb87433
Harshitha <harshithabn2003@gmail.com>
2024-08-05 10:24:28 +0530

    JAVA PROGRAMS

--- methods/MaskRunner.java
+++ methods/MaskRunner.java
@@ -11,16 +11,26 @@ class MaskRunner {
 		//System.out.println("cost  : "+mask.cost);
 		//System.out.println("size : "+mask.size);
 		//System.out.println("material  : "+mask.material);
+		Mask mask2=new Mask(50);
+		Mask mask3=new Mask('L');
+		Mask mask4=new Mask("cotton");
+		Mask mask5=new Mask(100,'L');
+		
 		
 		System.out.println("--------------------------------------");
 		
 		Kerosene kerosene1=new Kerosene();
 		Kerosene kerosene=new Kerosene(80,99,true);
-		
+		Kerosene kerosene2=new Kerosene(false);
+		Kerosene kerosene3=new Kerosene(500);
+
+
 		System.out.println("-----------------------------------------");
 		
 		Aeroplane aeroplane1=new Aeroplane();
 		Aeroplane aeroplane=new Aeroplane("Aerospace",200,10000,"Bengaluru","Swizzarland");
+		Aeroplane aeroplane2=new Aeroplane("Airbus","Shivamoga","Mysore");
+		Aeroplane aeroplane3=new Aeroplane("AirIndia",90000,"Bengaluru","America");
 		
 		System.out.println("Ending in MaskRunner");
 	}
